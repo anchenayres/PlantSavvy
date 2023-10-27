578009c442c3480df75a6643cadd96d95ab81a4a
anchenayres <21100284@virtualwindow.co.za>
2023-10-27 23:22:23 +0200

    Errors
    Invariant Violation: "main" has not been registered. This can happen if:
    * Metro (the local dev server) is run from the wrong folder. Check if Metro is running, stop it and restart it in the current project.
    * A module failed to load due to an error and `AppRegistry.registerComponent` wasn't called., js engine: hermes

--- Screens/HomeScreen.jsx
+++ Screens/HomeScreen.jsx
@@ -6,7 +6,7 @@ import { Ionicons } from '@expo/vector-icons';
 import {useUserEmail} from '../Services/firebaseAuth';
 import { fetchUserImages } from '../Services/firebaseDb';
 import { useNavigation } from '@react-navigation/native';
-import * as FileSystem from 'expo-file-system';
+//import * as FileSystem from 'expo-file-system';
 import RNFS from 'react-native-fs'; //convert to base64 image
 
 //get data
@@ -59,10 +59,7 @@ const HomeScreen = () => {
     //navigate to plantDetailScreen NEED TO DO
     const handlePlantDetailNavigation = async (imageUri, latitude, longitude) => {
       try {
-        const imageBase64 = await FileSystem.readAsStringAsync(imageUri, {
-          encoding: FileSystem.EncodingType.Base64,
-        });
-    
+        const imageBase64 = await RNFS.readFile(imageUri, 'base64');    
         const plantDetails = await identifyPlant(imageBase64, latitude, longitude);
     
         if (plantDetails && plantDetails.result.is_plant) {
