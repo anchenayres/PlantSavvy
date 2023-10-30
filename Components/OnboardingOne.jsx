e29657556c0ac38d6c17f1207b35582be776edc2
anchenayres <21100284@virtualwindow.co.za>
2023-10-30 08:54:41 +0200

    OboardingOne
    * Images added
    * Styling the text

--- Components/OnboardingOne.jsx
+++ Components/OnboardingOne.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, Button, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
 const OnboardingOne = ({ navigation }) => {
   // Handle navigation to the next screen
@@ -9,6 +9,11 @@ const OnboardingOne = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
+      <Image
+        source={require("../assets/plantbackground.jpg")} // Check the path to your image
+        style={styles.logo}
+      />        
+
       <Text style={styles.title}>Welcome to PlantSavvy</Text>
       <Text style={styles.description}>
         Take controll of your plants. A complete health assessmnet will be given to ensure your plants quality. There is no time to waist so let's get started!
@@ -16,6 +21,12 @@ const OnboardingOne = ({ navigation }) => {
       <TouchableOpacity style={styles.nextButton} onPress={handleNext}>
                 <Text style={styles.buttonText}>Let's Go</Text>
         </TouchableOpacity>    
+
+        <Image
+        source={require("../assets/plantbackground.jpg")} // Check the path to your image
+        style={styles.logo2}
+      />        
+
 
     </View>
   );
@@ -27,26 +38,43 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     paddingHorizontal: 20,
-    backgroundColor: '#69A579',
+    backgroundColor: 'white',
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
-    color: 'white',
+    color: '#0B4D21',
   },
   description: {
     fontSize: 16,
     textAlign: 'center',
     marginBottom: 30,
-    color: 'white',
+    color: '#5A5A5A',
 
   },
+  logo: {
+    height: 120,
+    width: 200,
+    top: -150,
+    left: -120,
+    borderTopRightRadius: 200,
+    borderBottomRightRadius: 200,
 
+  },
+  logo2: {
+    height: 100,
+    width: 200,
+    top: 190,
+    right: -120,
+    borderTopLeftRadius: 200,
+    borderBottomLeftRadius: 200,
+
+  },
   nextButton: {
     backgroundColor: "#0B4D21",
     borderRadius: 5,
-    marginTop: 80,
+    top: 50,
     padding: 10,
   },
   buttonText: {
